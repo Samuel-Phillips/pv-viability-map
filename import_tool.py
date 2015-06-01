3b48e49112b7a45bc6213e2018957bf77042cdcc
Samuel Phillips <samtheman@localhost>
2015-06-01 15:47:46 -0400

    Forced those coordinate conversions in but IT STILL DOSNT FUCKING WORK

--- import_tool.py
+++ import_tool.py
@@ -56,25 +56,27 @@ def import_shape_file(saveable, db, proj):
 
 def perform_import(sf, proj, db):
     """Takes a pyshp instance and imports its point to the database."""
-    cols = {n: None for n in 'kwhs BuidArea Perc Savings UseRoof Zone'.split()}
+    cols = {n: None for n in 'kwhs BuidArea Perc System Savings UseRoof Zone'.split()}
     for i, f in enumerate(sf.fields):
         if f[0] in cols:
-            cols[f[0]] = i
-
-    if kwhs_col < 0:
-        kwhs_col = 0
+            cols[f[0]] = i - 1
     try:
         db.add_rects(
-                pginterface.Rect(
-                    wktshape=points2wkt(row.shape.points, proj),
-                    sunlight=row.record[kwhs_col]
-                ) for row in sf.shapeRecords() if is_useful(row)
+            pginterface.Rect(
+                wktshape=points2wkt(row.shape.points, proj),
+                building_area=row.record[cols['BuidArea']],
+                useable_build_area=row.record[cols['UseRoof']],
+                percent_usable=row.record[cols['Perc']],
+                kwhs=row.record[cols['kwhs']],
+                system_size_kw=row.record[cols['System']],
+                savings=int(100 * float(row.record[cols['Savings']]))
+            ) for row in sf.shapeRecords() if is_useful(row)
         )
     except:
         traceback.print_exc()
         raise error("Database error, see log")
 
-def points2wkt(points):
+def points2wkt(points, proj):
     """Converts a list of points into a WKT polygon."""
     points.append(points[0]) # work around for polygons not being connected
     outproj = pyproj.Proj('+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')
@@ -82,12 +84,18 @@ def points2wkt(points):
     
     return "POLYGON(({}))".format(
             ','.join(
-                ' '.join(str(dim) for dim in pyproj.transform(
-                    inproj, outproj, point)[:2] for point in points
+                ' '.join(str(test(dim, point)) for dim in pyproj.transform(
+                    inproj, outproj, *point)[:2]
+                ) for point in points
             ))
 
 def is_useful(row):
     return True
+
+def test(dim, point):
+    if str(dim) == 'inf':
+        print("Shit, let's be {}".format(repr(point)))
+    return dim
 
 class error(Exception):
     """Generic error from the import process that contains a human readable
